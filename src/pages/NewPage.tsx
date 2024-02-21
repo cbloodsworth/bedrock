6d05031aec555705fa12aa0906ca5120e9b93e96
Jonathan Levy <jonathanlevy2002@gmail.com>
2024-02-21 15:51:30 -0500

    revert NewPage changes

--- src/pages/NewPage.tsx
+++ src/pages/NewPage.tsx
@@ -1,25 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NewPage: React.FC = () => {
-  const [data, setData] = useState("");
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/backend")
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error));
-  }, []);
-  return (
-    <div>
-      <h1>
-        <Link to="/">Home</Link>
-      </h1>
-      <div>
-        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>
+            <Link to="/">Home</Link>
+            </h1>
+        </div>
+    );
 };
-
+ 
 export default NewPage;
