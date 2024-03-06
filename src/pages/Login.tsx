dc2868f9c9e4ab0b416ea45801af9867a68cee66
Jonathan Levy <jonathanlevy2002@gmail.com>
2024-03-05 19:11:02 -0500

    fixed incorrect port

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ const Login: React.FC = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = `http://${window.location.hostname}:5000/loginGoogle`;
+    window.location.href = `http://${window.location.hostname}:5173/loginGoogle`;
   };
 
   const handleLinkedinLogin = () => {
