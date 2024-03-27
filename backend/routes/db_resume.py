621be6c7902fb9064f1daf13f78cfd3afca53cde
Chris Bloodsworth <christopherbloodsworth@gmail.com>
2024-03-27 18:45:42 -0400

    Added docstrings for all resume routes

--- backend/routes/db_resume.py
+++ backend/routes/db_resume.py
@@ -17,6 +17,13 @@ def createResume():
     Requires: 
         Params: user_id
         JSON Body: resume
+
+    Purpose:
+        Pushes a resume to the database under the user referenced by user_id.
+        Does not require resume_id, one is generated.
+        Returns the json representation of inserted resume on success. This representation will 
+            contain generated IDs for every child object (sections, entries, bullet points.)
+        Not idempotent: does not check for duplicates in any way, will always add a new resume on success.
     """
 
     json_resume = request.get_json()
@@ -39,7 +46,14 @@ def createResume():
 def readResume():
     """ 
     Requires: 
-        Params: user_id AND/OR entry_id
+        Params: user_id AND/OR resume_id
+
+    Purpose:
+        Returns a resume or array of resumes based on input.
+        If a resume id is provided, then a json representation of that resume is returned.
+        If a user id is provided but not a resume id, then it will return *all* resumes
+            associated with that user.
+        This operation is idempotent and has no side-effects.
     """
     user_id = request.args.get('user_id')
     resume_id = request.args.get('resume_id')
@@ -68,6 +82,17 @@ def updateResume():
     Requires: 
         Params: resume_id
         JSON Body: resume
+
+    Purpose:
+        Updates a resume in the database to the provided json body.
+        A resume_id is required -- whatever resume this refers to is deleted, with the new
+            resume inserted in its place.
+        Not technically idempotent since new IDs are generated every time. If you are using
+            this route, make sure to take note of the new IDs for the resume, sections, 
+            entries, and bullet points -- the new json representation is returned on success.
+        >>NOTE THAT THIS ROUTE IS NOT PERFORMANT - It may take anywhere from 3-12 seconds per run.
+            If an update is required on a resource, prefer updating that specific resource
+            through its own route: e.g., /db/entry/update for entries.
     """
     resume_id = request.args.get('resume_id')
     if (error := validate_params({'resume_id':resume_id}, "deleteResume")) is not None:
@@ -97,6 +122,11 @@ def deleteResume():
     """ 
     Requires: 
         Params: resume_id
+
+    Purpose:
+        Deletes the resume associated with the provided resume_id.
+        If no resume in the database has the provided resume_id, an error is thrown.
+        Not idempotent.
     """
     resume_id = request.args.get('resume_id')
 
